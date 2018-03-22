291f4e337cf466a8069647fe0060a93f7780b132
admblack97 <admblack97@gmail.com>
2018-03-22 22:20:29 +0800

    membuat class aksi dan main

---
+++ src/class_printer/printerAksi.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package class_printer;
+
+/**
+ *
+ * @author Adam Morseleno
+ */
+public class printerAksi {
+    public static void main(String[] args) {
+        
+    }
+}
